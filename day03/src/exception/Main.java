0ffe97dc57f9f665ec5aff79096788862360db2d
gudru <gudrud852@gmail.com>
2025-07-07 11:27:32 +0900

    0

---
+++ day03/src/exception/Main.java
@@ -0,0 +1,30 @@
+package exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Input Number..?");
+        Scanner scanner = new Scanner(System.in);
+        int num = 0;
+        try{
+            num = scanner.nextInt();
+        } catch(InputMismatchException e){
+            System.out.println("Please enter a number.");
+            return;
+        }
+        int a[] = null;
+        try{
+            a = new int[num];
+        }catch(Exception e){
+            System.out.println("Please enter a Positive Number.");
+            return;
+        }
+        for (int i = 0; i <= a.length; i++) {
+            System.out.printf("%d", a[i]);
+        }
+    }
+}
